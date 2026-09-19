19ab6e38d514f36c7a94624762785bf7fb2ff166
DESKTOP-KRLINVT\PRAVALIKA <erukullapravalika2911999@gmail.com>
2026-09-19 17:14:58 +0530

    Resolve circular dependency using @PostConstruct

--- src/main/java/com/example/SpringDemoApplication/Component/Order.java
+++ src/main/java/com/example/SpringDemoApplication/Component/Order.java
@@ -8,14 +8,14 @@ public class Order {
 	
 	User user;
 	
-	/* order is dependent on user
-	 * user is dependent on order 
-	 * This is known as circular dependency
-	 */
-	@Autowired
-	public Order(User user) {
-		this.user=user;
+	public Order() {
 		System.out.println("Initialized order");
+	}
+
+	public void setOrder(User user) {
+		this.user=user;
+		
+		
 	}
 
 }
--- src/main/java/com/example/SpringDemoApplication/Component/User.java
+++ src/main/java/com/example/SpringDemoApplication/Component/User.java
@@ -4,21 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+
 @Component
 public class User {
 	
-	Order order; 
 	@Autowired
-	/* @Lazy Injects beans whenever needed
-	 * Delays creation of one dependency 
-	 * Tells spring to give user a lazy reference/proxy to order and create order when it is actually needed
-	 * can add on field level, setter level as well
-	 */
-	@Lazy 
+	Order order; 
+	
 	public User(Order order) {
 		this.order=order;
 		System.out.println("Intialized user");
 	} 
+	
+	@PostConstruct
+	/* @PostConstruct is used to run a method after 
+	 * Spring has created the bean and completed its dependency injection.
+	 * After constructing user bean set it inside order
+	 */ 
+	public void init() {
+		order.setOrder(this);
+	}
 	
 	
 
