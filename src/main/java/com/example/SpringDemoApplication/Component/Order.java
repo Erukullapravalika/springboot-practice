8b8f5bb293559d1502360cdede07e2c682b01669
DESKTOP-KRLINVT\PRAVALIKA <erukullapravalika2911999@gmail.com>
2026-09-19 16:38:51 +0530

    Resolve circular dependency in dependency injection using @Lazy

--- src/main/java/com/example/SpringDemoApplication/Component/Order.java
+++ src/main/java/com/example/SpringDemoApplication/Component/Order.java
@@ -1,11 +1,20 @@
 package com.example.SpringDemoApplication.Component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Order {
 	
-	public Order() {
+	User user;
+	
+	/* order is dependent on user
+	 * user is dependent on order 
+	 * This is known as circular dependency
+	 */
+	@Autowired
+	public Order(User user) {
+		this.user=user;
 		System.out.println("Initialized order");
 	}
 
--- src/main/java/com/example/SpringDemoApplication/Component/User.java
+++ src/main/java/com/example/SpringDemoApplication/Component/User.java
@@ -1,32 +1,25 @@
 package com.example.SpringDemoApplication.Component;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
 	
-	//Field Injection-- Dependency is set into filed's of a class
-	@Autowired
 	Order order; 
-	OnlineOrder onlineorder;
-    
-	/* Constructor Injection-- Dependency will be resolved at time of initialization of object
-	 * Commonly Preferred Injection Type 
-	 * Fails at Compilation if there is any missing dependency
-	 */
 	@Autowired
-	public User(Order order, OnlineOrder onlineorder) {
+	/* @Lazy Injects beans whenever needed
+	 * Delays creation of one dependency 
+	 * Tells spring to give user a lazy reference/proxy to order and create order when it is actually needed
+	 * can add on field level, setter level as well
+	 */
+	@Lazy 
+	public User(Order order) {
 		this.order=order;
-		this.onlineorder=onlineorder;
 		System.out.println("Intialized user");
 	} 
 	
-	//Setter Injection- Dependency is set using setter of field
-	@Autowired
-	public void setOrder(Order order) {
-		this.order=order;
-		
-	}
+	
 
 }
