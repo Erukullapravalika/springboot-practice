619517edf36316ea2398a71061b94e7b68d94e3e
DESKTOP-KRLINVT\PRAVALIKA <erukullapravalika2911999@gmail.com>
2026-09-25 11:38:06 +0530

    Update application context handling

--- src/main/java/com/example/SpringDemoApplication/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemoApplication/SpringDemoApplication.java
@@ -6,18 +6,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-import com.EcomAssistantService.DeprecatedUtilityService;
 
 @SpringBootApplication
-// @ComponentScan scans the specified package and its subpackages
-@ComponentScan(basePackages={"com.EcomAssistantService", "com.example.SpringDemoApplication"},
-excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {DeprecatedUtilityService.class}))
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringDemoApplication.class, args);
 		System.out.print("Context Created");
-		applicationContext.close();
+		//applicationContext.close();
 	}
 
 }
